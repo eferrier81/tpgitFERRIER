19e6de3caeedcbd24416d1d89a918918c89ff136
Younes Nafaa <younes.nafaa@etud.univ-jfc.fr>
2024-10-16 09:46:13 +0200

    version2

--- src/main/java/org/github/ferrier/Main.java
+++ src/main/java/org/github/ferrier/Main.java
@@ -3,7 +3,7 @@ package org.github.ferrier;
 public class Main {
 
     public static void main(String[] args) {
-        // commentaire
+        // commentaire : tip top, tres bon code, je recommande
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
