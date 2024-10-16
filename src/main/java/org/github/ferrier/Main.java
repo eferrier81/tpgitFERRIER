b6c27c5328138154f272d8d49da40fe2f1f6ae90
Younes Nafaa <younes.nafaa@etud.univ-jfc.fr>
2024-10-16 09:52:58 +0200

    commit conflit

--- src/main/java/org/github/ferrier/Main.java
+++ src/main/java/org/github/ferrier/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // commentaire : tip top, tres bon code, je recommande
-        // 2ème commentaire
+        // pas terrible cette version...
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
