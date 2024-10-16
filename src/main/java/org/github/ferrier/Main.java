e4116242268acbd0d425f30d89082b9c0bf6d854
Enzo Ferrier <enzo.ferrier@etud.univ-jfc.fr>
2024-10-16 09:49:39 +0200

    3ème commit

--- src/main/java/org/github/ferrier/Main.java
+++ src/main/java/org/github/ferrier/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // commentaire : tip top, tres bon code, je recommande
+        // 2ème commentaire
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
